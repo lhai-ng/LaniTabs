1e1258714d440ec1cbe541bd052fbd489c4ad825
Long Hai Nguyen <longhai14149@gmail.com>
2025-07-17 07:49:53 +0700

    Added Switch and Destroy methods

--- src/lanitabs.js
+++ src/lanitabs.js
@@ -26,29 +26,27 @@ function LaniTabs(selector) {
         })
         .filter(Boolean);
     if (this.tabs.length !== this.panels.length) return;
-
+    
+    this._originalTabs = this.container.innerHTML;
     this._init();
 }
 
 LaniTabs.prototype._init = function () {
-    const tabActive = this.tabs[0];
-    tabActive.closest('li').classList.add('lanitabs--active');
-
-    this.panels.forEach(panel => panel.hidden = true);
+    this._activeTab(this.tabs[0]);
 
     this.tabs.forEach(tab => {
         tab.onclick = (event) => {
             this._handleTabsClick(event, tab);
         }
     })
-
-    const panelActive = this.panels[0];
-    panelActive.hidden = false;
 }
 
 LaniTabs.prototype._handleTabsClick = function (event, tab) {
     event.preventDefault();
+    this._activeTab(tab);
+}
 
+LaniTabs.prototype._activeTab = function (tab) {
     this.tabs.forEach(tab => {
         tab.closest('li').classList.remove('lanitabs--active');
     });
@@ -58,4 +56,29 @@ LaniTabs.prototype._handleTabsClick = function (event, tab) {
     this.panels.forEach(panel => panel.hidden = true);
     const panelActive = document.querySelector(tab.getAttribute('href'));
     panelActive.hidden = false;
+}
+
+LaniTabs.prototype.switch = function (input) {
+    let tabToActive = null;
+
+    if (typeof input === 'string') {
+        tabToActive = this.tabs.find(tab => tab.getAttribute('href') === input);
+    } else if (this.tabs.includes(input)) {
+        tabToActive = input;
+    }
+
+    if (!tabToActive) {
+        console.error(`LaniTabs: No tab found with: ${input}`);
+        return;
+    }
+
+    this._activeTab(tabToActive);
+}
+
+LaniTabs.prototype.destroy = function () {
+    this.container.innerHTML = this._originalTabs;
+    this.panels.forEach(panel => panel.hidden = false);
+    this.container = null;
+    this.tabs = null;
+    this.panels = null;
 }
