e7fc2d430409dfb63b1af14734ebbdd8bce99e1c
Long Hai Nguyen <longhai14149@gmail.com>
2025-07-20 15:04:33 +0700

    Optimize logic for LaniTabs

--- src/lanitabs.js
+++ src/lanitabs.js
@@ -11,70 +11,72 @@ function LaniTabs(selector, options = {}) {
         return;
     }
     
-    this.panels = this.tabs
-        .map(tab => {
-            const panel = document.querySelector(tab.getAttribute('href'));
-            if (!panel) {
-                console.error(`LaniTabs: No panel found for selector: '${tab.getAttribute('href')}'`);
-                
-            }
-            return panel;
-        })
-        .filter(Boolean);
+    this.panels = this._getPanels();
     if (this.tabs.length !== this.panels.length) return;
     
-    this._paramKey = selector.replace(/[^a-zA-Z0-9]/g, '');
+    this._cleanRegex = /[^a-zA-Z0-9]/g;
+    this._paramKey = selector.replace(this._cleanRegex, '');
     this.opt = Object.assign({
         remember: false,
         onChange: null,
+        activeCssClass: 'lanitabs--active',
     }, options);
 
     this._init();
+}
+
+LaniTabs.prototype._getPanels = function () {
+    return this.tabs
+            .map(tab => {
+                const panel = document.querySelector(tab.getAttribute('href'));
+                if (!panel) {
+                    console.error(`LaniTabs: No panel found for selector: '${tab.getAttribute('href')}'`);
+                    
+                }
+                return panel;
+            })
+            .filter(Boolean);
 }
 
 LaniTabs.prototype._init = function () {
     const params = new URLSearchParams(location.search);
     const tabSelector = params.get(this._paramKey);
     const tab = 
-        (this.opt.remember && tabSelector && this.tabs.find((tab) => tab.getAttribute('href').replace(/[^a-zA-Z0-9]/g, '') === tabSelector)) || 
+        (this.opt.remember && tabSelector && this.tabs.find((tab) => tab.getAttribute('href').replace(this._cleanRegex, '') === tabSelector)) || 
         this.tabs[0];
-    this._currentTab = tab;
+    this.currentTab = tab;
     
-    this._activateTab(tab, false);
+    this._activateTab(tab, false, false);
 
     this.tabs.forEach(tab => {
         tab.onclick = (event) => {
-            this._handleTabsClick(event, tab);
+            event.preventDefault();
+            this._tryActivateTab(tab);
         }
     })
 }
 
-LaniTabs.prototype._handleTabsClick = function (event, tab) {
-    event.preventDefault();
-    this._tryActivateTab(tab);
-}
-
 LaniTabs.prototype._tryActivateTab = function (tab) {
-    if (this._currentTab !== tab) {
+    if (this.currentTab !== tab) {
         this._activateTab(tab);
-        this._currentTab = tab;
+        this.currentTab = tab;
     }
 }
 
-LaniTabs.prototype._activateTab = function (tab, triggerOnChange = true) {
+LaniTabs.prototype._activateTab = function (tab, triggerOnChange = true, updateURL = true) {
     this.tabs.forEach(tab => {
-        tab.closest('li').classList.remove('lanitabs--active');
+        tab.closest('li').classList.remove(this.opt.activeCssClass);
     });
 
-    tab.closest('li').classList.add('lanitabs--active');
+    tab.closest('li').classList.add(this.opt.activeCssClass);
 
     this.panels.forEach(panel => panel.hidden = true);
     const panelActive = document.querySelector(tab.getAttribute('href'));
     panelActive.hidden = false;
 
-    if (this.opt.remember) {
+    if (this.opt.remember && updateURL) {
         const paramsSearch = new URLSearchParams(location.search);
-        paramsSearch.set(this._paramKey, tab.getAttribute('href').replace(/[^a-zA-Z0-9]/g, ''))
+        paramsSearch.set(this._paramKey, tab.getAttribute('href').replace(this._cleanRegex, ''))
         history.replaceState(null, null, `?${paramsSearch}`);
     }
 
@@ -109,4 +111,5 @@ LaniTabs.prototype.destroy = function () {
     this.container = null;
     this.tabs = null;
     this.panels = null;
+    this.currentTab = null
 }
